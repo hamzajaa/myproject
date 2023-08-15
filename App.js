3f3ab02f36b9a683cc2a10713064c364453d5c18
hamzajaa <hamzajaa2017@gmail.com>
2023-08-15 12:31:22 +0100

    change app

--- App.js
+++ App.js
@@ -4,17 +4,36 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+      <Text style={styles.boldText}>Hello, world! </Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum <Text>test</Text> dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ // object : key value
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body : {
+    backgroundColor: 'yellow',
+    padding: 20,
+    fontWeight: 'bold',
+  }
 });
