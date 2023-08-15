e02f2990b4d31645f4d1dfdb3f01b6aa2bb09454
hamzajaa <hamzajaa2017@gmail.com>
2023-08-15 13:19:49 +0100

    change 2.1

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, seName] = useState('hamza')
+
   return (
     <View style={styles.container}>
+      <Text>My name is {name}</Text>
       <Text></Text>
-      <Text></Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' />
+      </View>
     </View>
   );
 }
