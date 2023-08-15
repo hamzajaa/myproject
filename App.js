f339b4a4ac7ee10b95c777e2f3474a0baf476a88
hamzajaa <hamzajaa2017@gmail.com>
2023-08-15 13:49:39 +0100

    change 3.1

--- App.js
+++ App.js
@@ -22,6 +22,8 @@ export default function App() {
       placeholder='e.g 45'
       onChangeText={(val) => setAge(val)}
       />
+
+      <Text>name: {name} , age: {age}</Text>
     </View>
   );
 }
@@ -34,6 +36,10 @@ const styles = StyleSheet.create({ // object : key value
     justifyContent: 'center',
   },
   input: {
-    marginTop: 20,
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 });
