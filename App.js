89138c5772d3251bcfb8d1de33463a62e02b058d
hamzajaa <hamzajaa2017@gmail.com>
2023-08-15 13:22:13 +0100

    change 2.2

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  const [name, seName] = useState('hamza')
-
+  const [name, setName] = useState('hamza');
+  const [person, setPerson] = useState({name: 'mario', age: 40});
+  const handleClick = () => {
+    setName('salah');
+    setPerson({name: 'luigi', age:45});
+  }
   return (
     <View style={styles.container}>
       <Text>My name is {name}</Text>
-      <Text></Text>
+      <Text>His name is {person.name} and His age is {person.age}</Text>
       <View style={styles.buttonContainer}>
-        <Button title='update state' />
+        <Button title='update state' onPress={handleClick}/>
       </View>
     </View>
   );
@@ -21,5 +25,8 @@ const styles = StyleSheet.create({ // object : key value
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
   }
 });
